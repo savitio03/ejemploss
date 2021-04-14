c3d11c4f5470e4ca59029bf0a522e7eca281c926
savitio03 <80012054+savitio03@users.noreply.github.com>
2021-04-14 13:34:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,9 +19,9 @@ $numeroAleatorio3 = rand(0, 9);
 $letraAleatoria4 = chr(rand(ord($DesdeLetra), ord($HastaLetra)));
 $numeroAleatorio4 = rand(0, 9);
 $numero=$letraAleatoria1.$numeroAleatorio1.$letraAleatoria2.$numeroAleatorio2.$letraAleatoria3.$numeroAleatorio3.$letraAleatoria4.$numeroAleatorio4;
-             
+   ?>            
 echo "Los digitos ganadores del premio es = ".$numero;
-    ?>
+  
 </body>
 </html>
 
